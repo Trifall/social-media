514763a56995534639ba6272be5fdd35196c031e
Trifall <JerrenT@Trifall.com>
2023-10-20 21:10:02 -0400

    Initialize CreatePostModal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
+import { Dialog } from '@headlessui/react';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import { useState } from 'react';
+import { AiOutlinePlusSquare } from 'react-icons/ai';
+import Button from '../components/Button';
+import Modal from '../components/Modal';
 import { buildDbClient } from '../utils/dbClient';
 import { Framework } from './api/add-framework';
 
@@ -40,6 +45,8 @@ async function getData() {
 }
 
 export default function Home({ frameworks }: HomeProps) {
+	const [createPostModalOpen, setCreatePostModalOpen] = useState(false);
+
 	if (!frameworks) {
 		return <div>Loading...</div>;
 	}
@@ -53,7 +60,11 @@ export default function Home({ frameworks }: HomeProps) {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<main className='bg-inherit'>
+				<CreatePostModal createPostModalOpen={createPostModalOpen} setCreatePostModalOpen={setCreatePostModalOpen} />
 				<div className='mb-32 flex flex-col text-center lg:mb-0 lg:text-left'>
+					<Button className='w-48 border-2' onClick={() => setCreatePostModalOpen(true)}>
+						Create Post
+					</Button>
 					<div className='mt-20 overflow-x-auto rounded-lg border border-gray-200 w-[80vw] max-w-2xl'>
 						<table className='w-full divide-y-2 divide-gray-200 text-sm'>
 							<thead>
@@ -83,6 +94,7 @@ export default function Home({ frameworks }: HomeProps) {
 								))}
 							</tbody>
 						</table>
+						B
 					</div>
 				</div>
 			</main>
@@ -95,4 +107,44 @@ export const getServerSideProps: GetServerSideProps<{ frameworks: Framework[] }>
 	return {
 		props: { frameworks: data },
 	};
+};
+
+const CreatePostModal = ({
+	createPostModalOpen,
+	setCreatePostModalOpen,
+}: {
+	createPostModalOpen: boolean;
+	setCreatePostModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+	const handleSubmit = async () => {
+		// handle creation
+	};
+
+	return (
+		<Modal isOpen={createPostModalOpen} closeModal={() => setCreatePostModalOpen(false)}>
+			<Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-gray-700 p-6 text-left align-middle shadow-xl transition-all'>
+				<div className='flex items-center gap-3'>
+					<div className='p-2 rounded-full'>
+						<AiOutlinePlusSquare className='h-6 w-6 text-teal-300' />
+					</div>
+					<Dialog.Title as='h3' className='text-lg font-medium leading-6 text-white'>
+						Create Post
+					</Dialog.Title>
+				</div>
+				<div className='mt-2'>
+					<p className='text-sm text-gray-200'>Fill out information for your post.</p>
+				</div>
+				{/* <div className='w-full border-t border-gray-400 my-4' /> */}
+
+				<div className='mt-4 flex flex-col gap-4'>
+					<Button
+						className='flex items-center shadow-sm justify-center gap-2 border-white border hover:text-white hover:bg-gray-800 transition-all duration-300'
+						onClick={() => handleSubmit()}
+					>
+						Submit
+					</Button>
+				</div>
+			</Dialog.Panel>
+		</Modal>
+	);
 };
