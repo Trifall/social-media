aa7c4d0886cae2d4860d271a4f0480187cc64af1
Dolan Reynolds <reynoldsdolan@gmail.com>
2023-10-27 00:47:02 -0400

    styling, liked button skeleton

---
+++ src/components/LikeButton.tsx
@@ -0,0 +1,12 @@
+// const LikeButton = ({
+//     onClick,
+//     liked,
+// }: {
+//     onClick: () => void;
+//     liked: boolean;
+// }) => {
+
+//     return (
+
+//     );
+// }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,7 @@ export default function Home({ posts }: HomeProps) {
 			</Head>
 			<main className='flex items-center justify-center bg-inherit'>
 				<CreatePostModal createPostModalOpen={createPostModalOpen} setCreatePostModalOpen={setCreatePostModalOpen} />
-				<div className='mb-32 flex max-w-[80vw] items-center justify-center text-center lg:mb-0 lg:text-left'>
+				<div className='mb-32 flex max-w-[80vw] items-center justify-center lg:mb-0 lg:text-left'>
 					<div className='relative flex max-h-[80vh] flex-col items-center gap-4 overflow-y-scroll'>
 						<Button
 							className='w-48  items-center rounded-lg bg-neutral-300 px-4 py-3 text-black transition-all duration-500 hover:bg-neutral-400 dark:bg-neutral-600 dark:text-white dark:hover:bg-gray-500 lg:px-3 lg:py-2'
@@ -93,8 +93,8 @@ export default function Home({ posts }: HomeProps) {
 											);
 										})}
 									</div>
-									<div className='flex flex-row flex-wrap justify-between'>
-										<div className='pl-6 flex flex-row gap-2 justify-center align-middle items-center'>
+									<div className='flex flex-row flex-wrap justify-between px-3 pt-3'>
+										<div className='flex flex-row gap-2 justify-center align-middle items-center'>
 											<button className='h-min w-min items-center rounded-lg bg-transparent p-0 align-middle text-black transition-all duration-500 dark:bg-transparent dark:text-white'>
 												<HiOutlineChatBubbleOvalLeft className='h-8 w-8 p-0 hover:fill-blue-500' />
 											</button>
